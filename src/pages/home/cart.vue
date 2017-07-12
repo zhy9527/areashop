6a3b9e5efec7aaf34170f8d5284f59bd39ebd77b
727177817 <727177817@qq.com>
2017-07-13 00:17:33 +0800

    cart

--- src/pages/home/cart.vue
+++ src/pages/home/cart.vue
@@ -3,7 +3,11 @@
         <div class="item">
             <mt-cell-swipe
                 v-for="item in 4"
-                :key="item.id"
+                :key="item.id" 
+                :left="[{
+                    content: '删除',
+                    style: { display: 'none'}
+                }]"  
                 :right="[{
                     content: '删除',
                     style: { background: 'red', color: '#fff', 'line-height': '90px' },
@@ -11,24 +15,28 @@
                 }]">
                 <div class="goods">
                     <div class="check">
-                        <svg class="icon" aria-hidden="true">
+                        <svg class="icon" :class="{ checked: item ==3 }" aria-hidden="true">
                             <use xlink:href="#icon-duihao"></use>
                         </svg>
                     </div>
                     <img src="https://imgjd3.fruitday.com/images/product_pic/2163/1/1-370x370-2163-4UA8R1KX.jpg" alt="">
                     <dl>
-                        <dt>明治保加利亚LB81酸奶明治保加利亚LB81酸奶明治保加利亚LB81酸奶(清甜原味)</dt>
+                        <dt>{{item}}(清甜原味)</dt>
                         <dd>180克</dd>
                         <div><strong>￥18.00</strong></div>
                     </dl>
                     <div class="qty">
-                        <svg class="icon" aria-hidden="true">
-                            <use xlink:href="#icon-jianhao"></use>
-                        </svg>
+                        <em>
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-jianhao"></use>
+                            </svg>
+                        </em>
                         <span> 12 </span>
-                        <svg class="icon" aria-hidden="true">
-                            <use xlink:href="#icon-add"></use>
-                        </svg>
+                        <em>
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-add"></use>
+                            </svg>
+                        </em>
                     </div>   
                 </div>                 
             </mt-cell-swipe>
@@ -66,68 +74,105 @@ export default {
 }
 </script>
 
+<style lang="scss">
+.mint-cell-title{
+    display: none;
+}
+.mint-cell-value{
+    width: 100%;
+}
+.main-nav{
+   
+}
+</style>
 <style scoped lang="scss">
-    @import '../../assets/scss/mixin/mixins';
-    .item{
-        
-        position: relative;
+@import '../../assets/scss/mixin/mixins';
+.has-footer{
+    margin-bottom: 1rem;
+}
+
+.item{
+    
+    position: relative;
+}
+.mint-cell-title{
+    display: none;
+}
+.goods{
+    position: relative;
+    @include display;
+    @include align-items;
+    padding: .1rem 0;
+    width: 100%;
+    img{
+        width: .8rem;
+        height: .8rem;
+        margin: 0 .08rem;
+        border-radius: .04rem;
     }
-    .goods{
-        position: relative;
-        @include display;
-        @include align-items;
-        padding: .1rem 0;
-        img{
-            width: .8rem;
-            height: .8rem;
-            margin: 0 .08rem;
-            border-radius: .04rem;
-        }
-        dl{
-            @include flex;
-            div{
-                line-height: 1;
-            }
-        }
-        dt{
-            @include ellipsis(2);
-            color: $gray;
-            font-size: .12rem;
-            height: .35rem;
-            line-height: .16rem;
-        }
-        dd{
-            @include ellipsis;
-            color: $gray-light;
-            height: .3rem;
-            line-height: .3rem;
-            font-size: .11rem;
-        }
-        strong{
-            color: $primary;
-            font-weight: normal;
+    dl{
+        @include flex;
+        div{
+            line-height: 1;
         }
     }
-    .qty{
+    dt{
+        @include ellipsis(2);
+        color: $gray;
+        font-size: .12rem;
+        height: .35rem;
+        line-height: .16rem;
+    }
+    dd{
+        @include ellipsis;
+        color: $gray-light;
+        height: .3rem;
+        line-height: .3rem;
+        font-size: .11rem;
+    }
+    strong{
+        color: $primary;
+        font-weight: normal;
+    }
+}
+.qty{
+    position: absolute;
+    bottom: 0;
+    right: 0rem;
+    text-align: center;
+    @include display;
+    em{
+        padding: .1rem;
+    }
+    span{
         position: absolute;
-        bottom: .1rem;
-        right: 0rem;
+        top:0;
+        left: 0;
+        width: 100%;
         text-align: center;
-        @include display;
-        span{
-            @include flex;
-            line-height: .24rem;
-            padding: 0 .06rem;
-            font-size: .13rem;
-        }
+        @include flex;
+        line-height: .42rem;
+        font-size: .13rem;
     }
-    .icon {
-        width: .18rem; 
-        height: .18rem;
-        overflow: hidden;
-        fill: $primary;
-        padding: .02rem;
-        border-radius: 100%;
-        border:.01rem solid $line;
+}
+.icon {
+    width: .18rem; 
+    height: .18rem;
+    overflow: hidden;
+    fill: $primary;
+    border-radius: 100%;
+    border:.01rem solid $line;
+}
+.check{
+    padding: .1rem;
+    .icon{
+        width: .14rem;
+        height: .14rem;
     }
+    .checked{
+        background: $primary;
+        border:.01rem solid $primary;
+        fill: #fff;
+    }
+}
 </style>
